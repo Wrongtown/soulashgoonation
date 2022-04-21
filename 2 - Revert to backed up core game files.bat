e8f34c64aa577a2a907c0fa9f4df4a5e719b5529
Wrongtown <troy@wrongtown.org>
2022-04-21 12:53:07 +0800

    Batch files to backup & restore core game files
    Make copying monsters.png easy on a windows machine while also avoiding spoiling any surprise in the changed asset.

---
+++ 2 - Revert to backed up core game files.bat
@@ -0,0 +1,14 @@
+@echo off
+echo.
+echo.
+echo Press any key to remove Goombanistan assets and restore existing core game files.
+echo.
+echo Press Ctrl+C if you need to terminate this process.
+echo.
+@echo on
+@echo off 
+pause
+
+copy ..\..\Assets\gfx\monsters.bak ..\..\Assets\gfx\monsters.png
+
+pause
