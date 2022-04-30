03319766a49aa625ae684a7ce2a3f5b9344b11b8
Wrongtown <git@wrongtown.org>
2022-04-30 21:22:58 +0800

    Amending .bat files to cover ..\core_gfx\assets\gfx\Untitled-1.png

--- wrongtown_goombanistan/1 - Backup core game files and replace.bat
+++ wrongtown_goombanistan/1 - Backup core game files and replace.bat
@@ -1,7 +1,7 @@
 @echo off
 echo.
 echo.
-echo Press any key to make backup of existing game file.
+echo Press any key to make backup of existing game files.
 echo.
 echo Press Ctrl+C if you need to terminate this process.
 echo.
@@ -10,11 +10,12 @@ echo.
 pause
 
 copy ..\..\..\Assets\gfx\monsters.png ..\..\..\Assets\gfx\monsters.bak
+copy ..\core_gfx\assets\gfx\Untitled-1.png ..\core_gfx\assets\gfx\Untitled-1.bak
 
 @echo off
 echo.
 echo.
-echo Press any key to replace backed-up game file with Goombanistan file.
+echo Press any key to replace backed-up game file with Goombanistan files.
 echo.
 echo Press Ctrl+C if you need to terminate this process.
 echo.
@@ -23,4 +24,5 @@ echo.
 pause
 
 copy assets\gfx\monsters.png ..\..\..\Assets\gfx\monsters.png
+copy assets\gfx\Untitled-1.png ..\core_gfx\assets\gfx\Untitled-1.png
 pause
--- wrongtown_goombanistan/2 - Revert to backed up core game files.bat
+++ wrongtown_goombanistan/2 - Revert to backed up core game files.bat
@@ -10,5 +10,5 @@ echo.
 pause
 
 copy ..\..\..\Assets\gfx\monsters.bak ..\..\..\Assets\gfx\monsters.png
-
+copy ..\core_gfx\assets\gfx\Untitled-1.bak ..\core_gfx\assets\gfx\Untitled-1.png
 pause
