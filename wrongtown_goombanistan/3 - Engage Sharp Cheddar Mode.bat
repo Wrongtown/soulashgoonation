c5c68249864fcbc18432438e89f261e2e86ea99b
Wrongtown <troy@wrongtown.org>
2023-02-10 18:59:31 +0800

    Resolving #61 make benefit glorious dictator for life Goombanna

--- wrongtown_goombanistan/3 - Engage Sharp Cheddar Mode.bat
+++ wrongtown_goombanistan/3 - Engage Sharp Cheddar Mode.bat
@@ -3,7 +3,7 @@ echo.
 echo.
 echo Press any key to make backup of existing Goombanistan game file in preparation for SHARP CHEDDAR MODE.
 echo.
-echo PLEASE NOTE: You will need to start a new game before these changes take effect. Saves from SHARP CHEDDAR MODE and normal Goombanistanian saves may be incompatible.
+echo PLEASE NOTE: You will need to start a new game before these changes take effect. Saves from SHARP CHEDDAR MODE and normal Goombanistinian saves may be incompatible.
 echo.
 echo Press Ctrl+C if you need to terminate this process.
 echo.
@@ -18,7 +18,7 @@ echo.
 echo.
 echo Press any key to replace backed-up game file with SHARP CHEDDAR MODE file.
 echo.
-echo PLEASE NOTE: You will need to start a new game before these changes take effect. Saves from SHARP CHEDDAR MODE and normal Goombanistanian saves may be incompatible.
+echo PLEASE NOTE: You will need to start a new game before these changes take effect. Saves from SHARP CHEDDAR MODE and normal Goombanistinian saves may be incompatible.
 echo.
 echo Press Ctrl+C if you need to terminate this process.
 echo.
--- wrongtown_goombanistan/4 - Disengage Sharp Cheddar Mode.bat
+++ wrongtown_goombanistan/4 - Disengage Sharp Cheddar Mode.bat
@@ -1,9 +1,9 @@
 @echo off
 echo.
 echo.
-echo Press any key to disengage SHARP CHEDDAR MODE and revert to the normal Goombanistanian mod.
+echo Press any key to disengage SHARP CHEDDAR MODE and revert to the normal Goombanistinian mod.
 echo.
-echo PLEASE NOTE: You will need to start a new game before these changes take effect. Saves from SHARP CHEDDAR MODE and normal Goombanistanian saves may be incompatible.
+echo PLEASE NOTE: You will need to start a new game before these changes take effect. Saves from SHARP CHEDDAR MODE and normal Goombanistinian saves may be incompatible.
 echo.
 echo Press Ctrl+C if you need to terminate this process.
 echo.
